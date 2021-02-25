7b9f5e0539a7708081bf6ae90b68290482e01e82
niklas gadeberg <nikl2207@tietgen.dk>
2021-02-25 14:48:48 +0100

    25-02-2021 14:45

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
 
 
 
-
-
+// var_dump($_POST);
+if(isset($_POST["create"]) && isset($_POST["username"])){
+    // make a new game room
+    print_r("<pre>Creating new game room</pre>");
+}
+else if(!isset($_POST["create"]) && isset($_POST["username"]) && isset($_POST["password"])){
+    // connect to a game room
+    print_r("<pre>Connecting to game room</pre>");
+}
 
 
 
--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,22 @@
-<main>
+<main id="index">
     <header>
         <h1>WAR</h1>
     </header>
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+        <div class="inputField">
+            <label for="username">Username</label>
+            <input type="text" require id="username" name="username" placeholder="Username">
+        </div>
+
+        <div class="inputField">
+            <label for="password">Join Room</label>
+            <input type="text" id="password" name="password" placeholder="Room Password">
+            <input type="submit" value="Join Room">
+        </div>
+
+        <div class="inputField">
+            <label for="create">Create Room</label>
+            <input type="submit" id="create" name="create" value="Create Room">
+        </div>
+    </form>
 </main>
